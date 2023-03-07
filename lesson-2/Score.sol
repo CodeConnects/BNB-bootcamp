bad969c97f39bbf6795e1d6478b382ccfb90908b
Scott Cielo <4674926+CodeConnects@users.noreply.github.com>
2023-03-07 15:09:57 -0500

    make score variable public to get an inferred read function which saves on gas and memory

--- lesson-2/Score.sol
+++ lesson-2/Score.sol
@@ -9,16 +9,24 @@ contract Score {
     *  so uint is the same as uint256
     *  but you can also make them smaller
     */
-    uint256 score;
+    uint256 public score;
 
     /*
     *  be sure to add a visibility to all function
     *  otherwise they will default to public and throw a warning
     *  options are public, private, internal and external
     */
-
+    
+    /*
+    *  by using inferred function, we save memory and gas
+    *  this getScore function can be inferred by making score variable public
     function getScore() public view returns (uint256) {
         return score;
+    }
+    */
+
+    function setScore(uint256 _newScore) public {
+        score = _newScore;
     }
 
 }
