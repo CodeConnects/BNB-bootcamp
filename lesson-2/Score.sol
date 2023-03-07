af892c66a2e0a56048e82399ea65f7f5ffef839e
Scott Cielo <4674926+CodeConnects@users.noreply.github.com>
2023-03-07 14:51:39 -0500

    Create Score.sol

---
+++ lesson-2/Score.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+contract Score {
+    
+    /* 
+    *  all variables in Solidity need a datatype
+    *  256 is the maximum size of an unisgned integer
+    *  so uint is the same as uint256
+    *  but you can also make them smaller
+    */
+    uint256 score;
+
+}
