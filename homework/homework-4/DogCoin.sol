a5d2bfb827394f0c97ce36a1ee05bbb419db9bbf
Scott Cielo <4674926+CodeConnects@users.noreply.github.com>
2023-03-09 15:59:25 -0500

    add totalSupply variable, constructor with 2,000,000 initial, and function to return

--- homework/homework-4/DogCoin.sol
+++ homework/homework-4/DogCoin.sol
@@ -6,5 +6,16 @@ pragma solidity 0.8.18;
 
 // 4. Create a contract called DogCoin.
 contract DogCoin {
+    // 5. Create a variable to hold the total supply, with an initial amount of 2 million
+    uint256 totalSupply;
+
+    constructor() {
+        totalSupply = 2000000;
+    }
+
+    // 6. Make a public function that returns the total supply.
+    function showTotalSupply() public view returns(uint256) {
+        return totalSupply;
+    }
 
 }
