42e66852d207e358acd91102745ab5023d83f854
Scott Cielo <4674926+CodeConnects@users.noreply.github.com>
2023-03-09 16:06:10 -0500

    add public function to increase supply and add address variable for owner

--- homework/homework-4/DogCoin.sol
+++ homework/homework-4/DogCoin.sol
@@ -9,13 +9,22 @@ contract DogCoin {
     // 5. Create a variable to hold the total supply, with an initial amount of 2 million
     uint256 totalSupply;
 
+    // 8. Declare an address variable called owner . this address will be allowed to change the total supply
+    address owner;
+
     constructor() {
         totalSupply = 2000000;
+        owner = msg.sender;
     }
 
     // 6. Make a public function that returns the total supply.
     function showTotalSupply() public view returns(uint256) {
         return totalSupply;
+    }
+
+    // 7. Make a public function that can increase the total supply in steps of 1000.
+    function increaseTotalSupply() public {
+        totalSupply = totalSupply + 1000;
     }
 
 }
