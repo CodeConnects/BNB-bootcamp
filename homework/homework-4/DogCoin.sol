0740f12c9e9276d4615cf88ebe773d03af92d158
Scott Cielo <4674926+CodeConnects@users.noreply.github.com>
2023-03-09 15:53:45 -0500

    create DogCoin contract

--- homework/homework-4/DogCoin.sol
+++ homework/homework-4/DogCoin.sol
@@ -1,4 +1,10 @@
+// 3. Before the pragma version, add a license identifier
 // SPDX-License-Identifier: UNLICENSED
 
 // 2. Define the pragma compiler version to 0.8.18
 pragma solidity 0.8.18;
+
+// 4. Create a contract called DogCoin.
+contract DogCoin {
+
+}
