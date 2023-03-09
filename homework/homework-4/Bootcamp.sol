24881d0d89f5aebf73d5bddc52ee62b988c8fb3c
Scott Cielo <4674926+CodeConnects@users.noreply.github.com>
2023-03-09 15:20:37 -0500

    add add variable, and update to owner address on deployment

--- homework/homework-4/Bootcamp.sol
+++ homework/homework-4/Bootcamp.sol
@@ -7,11 +7,17 @@ contract BootcampContract {
 
     uint256 number;
 
+    // 1. Add a variable to hold the address of the deployer of the contract
+    address owner;
+
+    // 2. Update that variable with the deployer's address when the contract is deployed.
+    constructor() {
+        owner = msg.sender;
+    }
 
     function store(uint256 num) public {
         number = num;
     }
-
 
     function retrieve() public view returns (uint256){
         return number;
