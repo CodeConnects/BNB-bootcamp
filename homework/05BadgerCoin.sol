094b72b9a7040a179ed7946615b5b5add9cd4f81
Scott Cielo <4674926+CodeConnects@users.noreply.github.com>
2023-03-13 16:55:54 -0400

    added decimals, changed supply math, and upgraded solidity version

--- homework/05BadgerCoin.sol
+++ homework/05BadgerCoin.sol
@@ -1,10 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+// import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
 contract BadgerCoin is ERC20 {
-    constructor(uint256 initialSupply) ERC20("BadgerCoin", "BC") {
-        _mint(msg.sender, initialSupply);
+    
+    uint256 bcSupply = 1000000;
+    uint256 bcDecimals = 18;
+
+    constructor() ERC20("BadgerCoin", "BC") {
+        _mint(msg.sender, bc2Supply * (10 ** bcDecimals) );
     }
 }
