30bfe2f43a1f270c1f1c74a817bdd2dfb583348e
Scott Cielo <4674926+CodeConnects@users.noreply.github.com>
2023-03-13 18:47:10 -0400

    deploy, verify and publish contract. Add address in comments

--- homework/05BadgerCoin.sol
+++ homework/05BadgerCoin.sol
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-// import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract BadgerCoin is ERC20 {
     
@@ -10,6 +9,9 @@ contract BadgerCoin is ERC20 {
     uint256 bcDecimals = 18;
 
     constructor() ERC20("BadgerCoin", "BC") {
-        _mint(msg.sender, bc2Supply * (10 ** bcDecimals) );
+        _mint(msg.sender, bcSupply * (10 ** bcDecimals) );
     }
 }
+
+// contract deployed, verified and published on BNB testnet
+// https://testnet.bscscan.com/address/0x7cc25886637447dce6c3b3b8d79d335e9c4c19e4#readContract
