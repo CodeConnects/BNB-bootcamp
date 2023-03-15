608c1d8ebaea7e531af61555c20b0208bd81581b
Scott Cielo <4674926+CodeConnects@users.noreply.github.com>
2023-03-15 15:11:35 -0400

    add initial code for NFT with mint and transfer

--- homework/06BadgerNFT.sol
+++ homework/06BadgerNFT.sol
@@ -1,1 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract BadgerNFT is ERC721 {
+    constructor() ERC721("Badger NFT", "BADGER") {}
+
+    function mint(address to, uint256 tokenId) public {
+        _mint(to, tokenId);
+    }
+
+    function transfer(address from, address to, uint256 tokenId) public {
+        safeTransferFrom(from, to, tokenId);
+    }
+}
