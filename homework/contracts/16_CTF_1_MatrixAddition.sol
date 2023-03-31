79a5b5d036c8966e61267f9fe577004bde31ab07
CodeConnects <codeconnects@gmail.com>
2023-03-31 15:15:38 -0400

    change for loop to inline assembly to save on gas

--- homework/contracts/16_CTF_1_MatrixAddition.sol
+++ homework/contracts/16_CTF_1_MatrixAddition.sol
@@ -8,14 +8,30 @@ pragma solidity 0.8.18;
 */
 
 contract Level_1_Solution {
-
-    function solution(uint256[2][2] calldata x, uint256[2][2] calldata y) external pure returns (uint256[2][2] memory finalArray) {
-        
-        // Loop through each element of the matrices and add them together
-        for (uint256 i = 0; i < 2; i++) {
-            for (uint256 k = 0; k < 2; k++) {
-                finalArray[i][k] = x[i][k] + y[i][k];
-            }
+  function solution(uint256[2][2] calldata x, uint256[2][2] calldata y) external pure returns (uint256[2][2] memory finalArray) {
+    // Loop through each element of the matrices and add them together
+    /*for (uint256 i = 0; i < 2; i++) {
+        for (uint256 k = 0; k < 2; k++) {
+            finalArray[i][k] = x[i][k] + y[i][k];
         }
+    }*/
+
+    // same loop but with inline assembly
+    assembly {
+      let i := 0
+      let k := 0
+
+      for { } lt(i, 2) { } {
+        for { } lt(k, 2) { } {
+          let x_i_k := mload(add(add(x, mul(i, 32)), mul(k, 32)))
+          let y_i_k := mload(add(add(y, mul(i, 32)), mul(k, 32)))
+          let sum := add(x_i_k, y_i_k)
+          mstore(add(add(finalArray, mul(i, 64)), mul(k, 32)), sum)
+          k := add(k, 1)
+        }
+        i := add(i, 1)
+        k := 0
+      }
     }
+  }
 }
