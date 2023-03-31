825ca162e78b45e813c148704eb41f75edeaac26
CodeConnects <codeconnects@gmail.com>
2023-03-31 14:35:50 -0400

    change file name of the example deploy script and make one for the CTF game

--- homework/scripts/deploy.js
+++ homework/scripts/deploy.js
@@ -1,32 +1,43 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const { deploy } = require('hardhat');
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  // Get the contracts to deploy
+  const Level_0_Solution = await ethers.getContractFactory("Level_0_Solution");
+  const level_0_Solution = await Level_0_Solution.deploy();
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
+  const Level_1_Solution = await ethers.getContractFactory("Level_1_Solution");
+  const level_1_Solution = await Level_1_Solution.deploy();
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  const Level_2_Solution = await ethers.getContractFactory("Level_2_Solution");
+  const level_2_Solution = await Level_2_Solution.deploy();
 
-  await lock.deployed();
+  const Level_3_Solution = await ethers.getContractFactory("Level_3_Solution");
+  const level_3_Solution = await Level_3_Solution.deploy();
 
-  console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  const Level_4_Solution = await ethers.getContractFactory("Level_4_Solution");
+  const level_4_Solution = await Level_4_Solution.deploy();
+
+  console.log("Contract 0 deployed to address:", level_0_Solution.address);
+  console.log("Contract 1 deployed to address:", level_1_Solution.address);
+  console.log("Contract 2 deployed to address:", level_2_Solution.address);
+  console.log("Contract 3 deployed to address:", level_3_Solution.address);
+  console.log("Contract 4 deployed to address:", level_4_Solution.address);
+
+  // Report gas usage
+  //const { gasUsed0 } = await level_0_Solution.deployTransaction.wait();
+  //console.log("Gas used for 0:", gasUsed0.toString());
+
+  //const { gasUsed1 } = await level_1_Solution.deployTransaction.wait();
+  //console.log("Gas used for 1:", gasUsed1.toString());
+
+  //const { gasUsed2 } = await level_2_Solution.deployTransaction.wait();
+  //console.log("Gas used for 2:", gasUsed2.toString());
+
+  //const { gasUsed3 } = await level_3_Solution.deployTransaction.wait();
+  //console.log("Gas used for 3:", gasUsed3.toString());
+
+  //const { gasUsed4 } = await level_4_Solution.deployTransaction.wait();
+  //console.log("Gas used for 4:", gasUsed4.toString());
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main();
